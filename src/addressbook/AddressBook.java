6b472c44aa67a76eb4c96d21b86833bc8064396e
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 19:09:43 +0530

    Added Code to create contacts

---
+++ src/addressbook/AddressBook.java
@@ -0,0 +1,60 @@
+package addressbook;
+
+public class AddressBook {
+	
+	private String firstName, lastName, city, state;
+	private int zipcode, phNo;
+	private String address, email;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstname) {
+		this.firstName = firstname;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastname) {
+		this.lastName = lastname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	public int getPhNo() {
+		return phNo;
+	}
+	public void setPhNo(int phNo) {
+		this.phNo = phNo;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+}
